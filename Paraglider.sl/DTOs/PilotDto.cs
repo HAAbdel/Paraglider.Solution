4cea6d8c7510a5f94fc2c0d1316ec1a71ac1d209
Abdel Hnini <Abdel.ali.hnini@hotmail.com>
2020-02-18 16:47:38 +0100

    :ssprkles: Dtos Query

---
+++ Paraglider.sl/DTOs/PilotDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Paraglider.sl.DTOs
+{
+    class PilotDto
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
