f6ae8e0339d43993ef0538654bcdaccb37660313
Abdel Hnini <Abdel.ali.hnini@hotmail.com>
2020-02-19 12:56:59 +0100

    :construction: Dtos

---
+++ Paraglider.sl/Queries/RoleList.cs
@@ -0,0 +1,26 @@
+﻿using Paraglider.DAL;
+using Paraglider.DAL.Models;
+using Paraglider.sl.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Paraglider.sl.Queries
+{
+    class RoleList
+    {
+        private readonly ParagliderContext _config;
+
+        public RoleList (ParagliderContext config)
+        {
+            _config = config;
+        }
+        public IQueryable<RoleDto> GetAllRoles()
+        {
+            var Roles = _config.Roles.Select(p => new RoleDto { Id = p.RoleId, RoleName = p.RoleName });
+
+            return Roles;
+        }
+    }
+}
--- ParagliderAPP/Startup.cs
+++ ParagliderAPP/Startup.cs
@@ -30,7 +30,6 @@ namespace ParagliderAPP
         {
             services.AddDbContext<ParagliderContext>(item => item.UseSqlServer(this.config.GetConnectionString("myconn")));
             services.AddControllersWithViews();
-     
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
