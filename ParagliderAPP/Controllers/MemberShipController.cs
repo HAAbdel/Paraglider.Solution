394c2a41525f0a8dcd26a7807a46a01a8b45d79a
Abdel Hnini <Abdel.ali.hnini@hotmail.com>
2020-03-10 16:21:09 +0100

    :construction: MemberShip

---
+++ ParagliderAPP/Controllers/MemberShipController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Paraglider.DAL;
+
+namespace ParagliderAPP.Controllers
+{
+    public class MemberShipController : Controller
+    {
+        private readonly ParagliderContext _context;
+        public MemberShipController(ParagliderContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
